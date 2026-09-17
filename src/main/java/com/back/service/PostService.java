0aa2c5d42f441a53876690d65fc5e4e6941c9a5d
Hyunsoo Song <gustn5522@gmail.com>
2026-09-17 13:30:43 +0900

    0004

--- src/main/java/com/back/service/PostService.java
+++ src/main/java/com/back/service/PostService.java
@@ -20,6 +20,9 @@ public class PostService {
     public Post write(Member author, String title, String content) {
         Post post = new Post(author, title, content);
 
+        author.increaseActivityScore(3);
+
+
         return postRepository.save(post);
     }
 
